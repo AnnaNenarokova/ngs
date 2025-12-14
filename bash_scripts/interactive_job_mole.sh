a15583d661cfb8c9fda9d583236273db3b0079b1
Anna <a.nenarokova@gmail.com>
2025-12-14 14:00:45 +0100

    ni

--- bash_scripts/interactive_job_mole.sh
+++ bash_scripts/interactive_job_mole.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-srun -N 1 --ntasks-per-node=1 --cpus-per-task=1 --mem=2G --time=99:99:99 --job-name=gtdb_unpack --pty bash
+srun -N 1 --ntasks-per-node=1 --cpus-per-task=1 --mem=2G --time=99:99:99 --job-name=test --pty bash
