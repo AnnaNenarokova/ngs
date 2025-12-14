034de4293ae819d6900cb118ec439f3af58b3645
Anna <a.nenarokova@gmail.com>
2025-12-14 13:41:44 +0100

    ni

--- cl103/run_write_marker_fastas.sh
+++ cl103/run_write_marker_fastas.sh
@@ -6,7 +6,8 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8G
 
-
+# Load Python
+PYTHON="/usr/local/bin/python"
 SCRIPT="/home/users/nenarokova/ngs/cl103/write_marker_fastas_hmmresults.py"
 
 TBL_DIR="/home/users/nenarokova/daria/cl103/phylogenetics/hmms/hmm_markers_results/"
@@ -16,7 +17,7 @@ OUT_DIR="/home/users/nenarokova/daria/cl103/phylogenetics/152_gtdb_markers/fasta
 echo "Job started on $(hostname)"
 echo "Start time: $(date)"
 
-python3 "$SCRIPT" \
+$PYTHON "$SCRIPT" \
   --tbl_dir "$TBL_DIR" \
   --proteomes_dir "$PROTEOMES_DIR" \
   --out_dir "$OUT_DIR" \
