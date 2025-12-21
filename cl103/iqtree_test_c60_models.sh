8cec25e786341a5bb65d3a50bee946583acd68e2
Anna <a.nenarokova@gmail.com>
2025-12-21 17:31:41 +0100

    ni

---
+++ cl103/iqtree_test_c60_models.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=iqtree_C60_MF
+#SBATCH --output=/home/users/nenarokova/slurm_output/iqtree_C60_MF_%A.out
+#SBATCH --time=7-12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=250G
+
+set -euo pipefail
+
+module load iqtree-2.1.0
+
+msa="/home/users/nenarokova/daria/cl103/phylogenetics/152_gtdb_markers/supermatrix/114_gtdb_markers_133_proteomes.fasta"
+
+iqtree2 -s "$msa" -T 8 -m MF -mset LG -madd C60 -mrate G --score-diff all -mem 250G -redo -pre C60_modeltest
